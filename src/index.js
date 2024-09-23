11d2037dec0ac4ceb0f755b5f473ea71d9f681b3
devpedrosena <pedrosenna540@gmail.com>
2024-09-23 10:36:02 -0300

    created playRaceEngine function responsible for a logic the game

--- src/index.js
+++ src/index.js
@@ -18,9 +18,19 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1
 }
 
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round <= 5; round++){
+        console.log(`🏁 Rodada: ${round}`)
+
+        //Sortear Bloco
+    }
+}
+
 (async function main() {
     console.log(
         `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando... \n`
     )
+
+    await playRaceEngine(player1.NOME, player2.NOME)
 })()
 
