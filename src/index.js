d0490efff664a014c9cd7055a12c7ffa3d6cfa8a
devpedrosena <pedrosenna540@gmail.com>
2024-09-23 09:50:56 -0300

    create index.js script and create two objects players

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+}
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+}
+
+
+
